184b36c7f255ebbad579e1c8ff1e466e2c2a91d3
Max Shabarchin <maks.shabarchin@yandex.ru>
2021-04-06 13:53:06 +0300

    Update AssemblyInfo.cs

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанных со сборкой.
 [assembly: AssemblyTitle("DOSBox Launcher 2.0")]
 [assembly: AssemblyDescription("The source code of the launcher for DOSBox in the PDukeFree 5")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zalexanninev15")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DOSBox Launcher 2.0")]
-[assembly: AssemblyCopyright("Copyright©  2019-2020, Zalexanninev15")]
-[assembly: AssemblyTrademark("DOSBox Launcher 2.0 (2019-2020)")]
+[assembly: AssemblyCopyright("GNU General Public License v3.0 by Zalexanninev15")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, следует установить атрибут ComVisible в TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("64ccca31-84c2-42ae-b655-4a0d1fdb5fa4")]
-
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyFileVersion("2.2.0.0")]
